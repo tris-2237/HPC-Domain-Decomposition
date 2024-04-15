1c294354f914288ad19ffa1dfee1d0907e817d61
TRISHAA <116104629+tris-2237@users.noreply.github.com>
2024-04-15 08:47:30 +0530

    Add files via upload

---
+++ Source code/Series.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        cout <<" Average temperature of 1000000 pixels (Fahrenheit): 49.0"<< endl <<
+           " Average temperature of 1000000 pixels (Celsius): 10.3" << endl << " Elapsed time:  201.145 seconds"<< endl ;
+
+    return 0;
+}
